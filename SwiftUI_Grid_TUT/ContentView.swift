bac09abcb6cc0507c74a9f43cb4cb8458b5b2697
SeonJungKim <ksj083895@gmail.com>
2021-02-05 17:24:22 +0500

    1. LazyVGrid

--- SwiftUI_Grid_TUT/ContentView.swift
+++ SwiftUI_Grid_TUT/ContentView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //목록을 1부터 1000까지 만듬
+    let data = Array(1...1000).map { "목록 \($0)"}
+    
+    //화면을 그리드형식으로 꽉채워줌
+    let layout = [
+        GridItem(.flexible(maximum: 80)),
+        GridItem(.flexible(maximum: 80)),
+        GridItem(.flexible(maximum: 80)),
+        GridItem(.flexible(maximum: 80))
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView() {
+            LazyVGrid(columns:layout, spacing: 20) {
+                ForEach(data, id: \.self) {i in
+                    VStack {
+                        Capsule()
+                            .fill(Color.yellow)
+                            .frame(height: 30)
+                        Text(i)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .padding(.horizontal)
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
